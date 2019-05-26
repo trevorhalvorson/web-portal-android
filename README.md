30e0c227781ab1b7e0705c1b34123c4748dea345
Trevor Halvorson <trevor@bymason.com>
2019-05-26 10:09:15 -0700

    Add support for uploading files through WebView
    Additional features:
    - Toolbar with refresh button and configurable title
    - Make applicationId (package name) configurable

--- README.md
+++ README.md
@@ -1,34 +1,33 @@
 # Portal
 
-A simple webview application that allows the options to be
-set by build parameters.
+A simple WebView application with configurations set by build parameters.
+Lock down a device to only display your website.
 
 ### Building
 
 Project Properties:
 
+- `applicationId`: Package name of the application
+- `versionCode`: Version code of the application. This must be incremented to perform app updates
+- `versionName`: Version name of the application.
 - `url`: The URL to load in the WebView
 - `allowBrowsing`: `true` if the user should be allowed to access hosts other than the given `url`'s host
-- `versionCode`: The Android App's version. This must be incremented to perform app updates
-
-Build an unsigned release APK:
-
-`./gradlew assembleRelease -Purl='"https://trevorhalvorson.com"' -PallowBrowsing=true -PversionCode=1 -PversionName='1.0.0'`
+- `toolbarTitle`: Text shown in the toolbar above the WebView
 
 Build a signed release APK:
 
-`KEYSTORE=keystore.jks KEYSTORE_PASSWORD=password KEY_ALIAS=key0 KEY_PASSWORD=password ./gradlew assembleRelease -Purl='"https://trevorhalvorson.com"' -PallowBrowsing=true PversionCode=1 -PversionName='1.0.0'`
+`KEYSTORE=keystore.jks KEYSTORE_PASSWORD=password KEY_ALIAS=key0 KEY_PASSWORD=password ./gradlew assembleRelease -PapplicationId='com.trevorhalvorson.portal' -PversionCode=1 -PversionName='1.0.0' -Purl='"https://www.wikipedia.org/"' -PallowBrowsing=true -PtoolbarTitle='"Portal"'`
 
-### Signing
+### Self-Signing
 
-(optional) Generating a Keystore:
+Generating a Keystore:
 
 `keytool -genkey -v -dname "cn=testcn, ou=testou, o=testo, c=US" -alias testalias -keypass testkeypass -keystore test.keystore -storepass teststorepass -keyalg RSA -keysize 2048 -validity 999`
 
-Sign APK with [apksigner](https://developer.android.com/studio/command-line/apksigner.html):
+Signing an APK with [apksigner](https://developer.android.com/studio/command-line/apksigner.html):
 
 `apksigner sign --ks test.keystore --in app/build/outputs/apk/release/app-release-unsigned.apk --out app-signed.apk`
 
-(optional) Verify signing:
+Verify signing:
 
 `apksigner verify -v --print-certs app-signed.apk`
--- app/src/main/kotlin/com/trevorhalvorson/portal/MainActivity.kt
+++ app/src/main/kotlin/com/trevorhalvorson/portal/MainActivity.kt
@@ -1,24 +1,51 @@
 package com.trevorhalvorson.portal
 
+import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
-import android.view.KeyEvent
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URI
+import android.view.Menu
+import android.view.MenuItem
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import java.io.File
+import android.support.v4.content.FileProvider
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.*
+import android.widget.Toast
 
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG: String = "MainActivity"
+        private const val PERMISSIONS_REQUEST_CODE = 1
+        private const val FILE_REQUEST_CODE = 2
+        private const val AUTHORITY = BuildConfig.APPLICATION_ID + ".provider"
+        private const val PHOTOS = "photos"
+        private val PERMISSIONS = arrayOf(Manifest.permission.INTERNET,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
 
-class MainActivity : Activity() {
+    private var filePathCallback: ValueCallback<Array<Uri>>? = null
+    private var output: File? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = BuildConfig.TOOLBAR_TITLE
 
         web_view.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?):
@@ -27,9 +54,43 @@ class MainActivity : Activity() {
                     val originalHost = URI(BuildConfig.URL).host
                     val requestedHost = URI(request!!.url.toString()).host
 
-                    return !originalHost.contentEquals(requestedHost)
+                    return !originalHost!!.contentEquals(requestedHost)
                 }
                 return false
+            }
+        }
+
+        web_view.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(webView: WebView?,
+                                           callback: ValueCallback<Array<Uri>>?,
+                                           fileChooserParams: FileChooserParams?): Boolean {
+                filePathCallback = callback
+                output = File(File(filesDir, PHOTOS),
+                        "${BuildConfig.APPLICATION_ID}_${System.currentTimeMillis()}.jpg")
+                if (output?.exists()!!) {
+                    output?.delete()
+                } else {
+                    output?.parentFile?.mkdirs()
+                }
+                val outputUri = FileProvider
+                        .getUriForFile(this@MainActivity, AUTHORITY, output!!)
+
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri)
+
+                val galleryIntent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                val fileIntent = Intent(Intent.ACTION_GET_CONTENT)
+                fileIntent.type = "*/*"
+
+                val chooserIntent = Intent
+                        .createChooser(cameraIntent, getString(R.string.file_intent_chooser_title))
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                        arrayOf(galleryIntent, fileIntent))
+                startActivityForResult(chooserIntent, FILE_REQUEST_CODE)
+
+                return true
             }
         }
 
@@ -40,11 +101,55 @@ class MainActivity : Activity() {
         if (savedInstanceState == null) {
             web_view.loadUrl(BuildConfig.URL)
         }
+
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+                break
+            }
+        }
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        web_view.saveState(outState)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        when (requestCode) {
+            FILE_REQUEST_CODE -> {
+                if (resultCode == RESULT_OK) {
+                    val outputUri: Uri = if (intent?.data != null) {
+                        // existing URI from storage
+                        intent.data!!
+                    } else {
+                        // URI we created
+                        FileProvider.getUriForFile(this, AUTHORITY, output!!)
+                    }
+                    filePathCallback?.onReceiveValue(arrayOf(outputUri))
+                } else {
+                    filePathCallback?.onReceiveValue(null)
+                }
+                filePathCallback = null
+            }
+            else -> {
+                Log.d(TAG, "Activity result for $requestCode not handled")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                for (result in grantResults) {
+                    if (result == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this,
+                                getString(R.string.permission_denied_message),
+                                Toast.LENGTH_LONG).show()
+                        break
+                    }
+                }
+            }
+            else -> {
+                Log.d(TAG, "Permissions result for $requestCode not handled")
+            }
+        }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
@@ -58,5 +163,20 @@ class MainActivity : Activity() {
             return true
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.refresh -> {
+                web_view.reload()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
